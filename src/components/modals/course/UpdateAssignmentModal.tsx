828b4500a1b5c7e1ae93bba60be3ceff850edbd5
Roberto M <robertom@Robertos-MBP.lan>
2024-04-05 13:47:38 -0400

    adds button styling

--- src/components/modals/course/UpdateAssignmentModal.tsx
+++ src/components/modals/course/UpdateAssignmentModal.tsx
@@ -9,62 +9,69 @@ import { useWallet } from "@meshsdk/react";
 import { AssetExtended } from "@meshsdk/core";
 import { andamioConfig } from "../../../andamio/config";
 
-const UpdateAssignmentModal = (props: { selectedModuleUTxO: CourseReferenceUTxO; learnerCS: string }) => {
-  const { connected, wallet } = useWallet();
-  const [connectedCourseCreator, setConnectedCourseCreator] = useState<AssetExtended | undefined>(undefined);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const UpdateAssignmentModal = (props: {
+	selectedModuleUTxO: CourseReferenceUTxO;
+	learnerCS: string;
+}) => {
+	const { connected, wallet } = useWallet();
+	const [connectedCourseCreator, setConnectedCourseCreator] = useState<
+		AssetExtended | undefined
+	>(undefined);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    async function getCourseCreator() {
-      if (connected) {
-        const _res = await getConnectedTokenAsset(wallet, andamioConfig.config.courseManagementTokens.courseCreatorPolicyID);
-        if (_res) {
-          setConnectedCourseCreator(_res);
-        }
-      }
-    }
+	useEffect(() => {
+		async function getCourseCreator() {
+			if (connected) {
+				const _res = await getConnectedTokenAsset(
+					wallet,
+					andamioConfig.config.courseManagementTokens.courseCreatorPolicyID
+				);
+				if (_res) {
+					setConnectedCourseCreator(_res);
+				}
+			}
+		}
 
-    if (connected) {
-      getCourseCreator();
-    }
-  }, [connected, wallet]);
+		if (connected) {
+			getCourseCreator();
+		}
+	}, [connected, wallet]);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 
-  return (
-    <div>
-      {connectedCourseCreator && (
-        <>
-          <button
-            onClick={openModal}
-            className="btn btn-primary"
-            type="button"
-          >
-            Update Assignment
-          </button>
-          <Modal
-            isOpen={isModalOpen}
-            closeModal={closeModal}
-            content={
-              <Suspense fallback={<Loading />}>
-                <UpdateAssignmentDatum
-                  selectedModuleUTxO={props.selectedModuleUTxO}
-                  newLearnerCS={props.learnerCS}
-                  closeModal={closeModal}
-                />
-              </Suspense>
-            }
-          />
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{connectedCourseCreator && (
+				<>
+					<button
+						onClick={openModal}
+						className='btn btn-primary rounded-md drop-shadow'
+						type='button'>
+						Update Assignment
+					</button>
+					<Modal
+						isOpen={isModalOpen}
+						closeModal={closeModal}
+						content={
+							<Suspense fallback={<Loading />}>
+								<UpdateAssignmentDatum
+									selectedModuleUTxO={props.selectedModuleUTxO}
+									newLearnerCS={props.learnerCS}
+									closeModal={closeModal}
+								/>
+							</Suspense>
+						}
+					/>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default UpdateAssignmentModal;

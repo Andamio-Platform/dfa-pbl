3ab35284249e5d1677c56a5cba99ab96e78841bc
Roberto M <robertom@Robertos-MBP.lan>
2024-04-05 13:35:39 -0400

    changes text from sec to primary

--- src/app/course-management/assignment-commitments/page.tsx
+++ src/app/course-management/assignment-commitments/page.tsx
@@ -5,14 +5,16 @@ import AssignmentCommitmentsTable from "./AssignmentCommitmentsTable";
 export const dynamic = "force-dynamic";
 
 export default async function AssignmentCommitmentsPage() {
-  const assignmentsTable = await AssignmentCommitmentsTable();
+	const assignmentsTable = await AssignmentCommitmentsTable();
 
-  return (
-    <main className="h-max min-h-screen">
-      <div className="flex flex-col py-10 text-secondary-content w-2/3 mx-auto my-10">
-        <h1 className="py-10 text-secondary-content text-4xl">Current Assignment Commitments</h1>
-        <Suspense fallback={<Loading />}>{assignmentsTable}</Suspense>
-      </div>
-    </main>
-  );
+	return (
+		<main className='h-max min-h-screen'>
+			<div className='flex flex-col py-10 text-secondary-content w-2/3 mx-auto my-10'>
+				<h1 className='py-10 text-primary-content text-4xl'>
+					Current Assignment Commitments
+				</h1>
+				<Suspense fallback={<Loading />}>{assignmentsTable}</Suspense>
+			</div>
+		</main>
+	);
 }

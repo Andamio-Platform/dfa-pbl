98375b12b4b1410bd4ebfd3470aab0539ed239d9
Roberto M <robertom@Robertos-MBP.lan>
2024-04-05 13:45:25 -0400

    adds button styling

--- src/components/modals/course/MintModuleTokenModal.tsx
+++ src/components/modals/course/MintModuleTokenModal.tsx
@@ -11,53 +11,64 @@ import { AssetExtended } from "@meshsdk/core";
 import { andamioConfig } from "../../../andamio/config";
 
 const MintModuleTokenModal = (props: { courseModule: CourseModule }) => {
-  const { connected, wallet } = useWallet();
-  const [connectedCourseCreator, setConnectedCourseCreator] = useState<AssetExtended | undefined>(undefined);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const { connected, wallet } = useWallet();
+	const [connectedCourseCreator, setConnectedCourseCreator] = useState<
+		AssetExtended | undefined
+	>(undefined);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    async function getCourseCreator() {
-      if (connected) {
-        const _res = await getConnectedTokenAsset(wallet, andamioConfig.config.courseManagementTokens.courseCreatorPolicyID);
-        if (_res) {
-          setConnectedCourseCreator(_res);
-        }
-      }
-    }
+	useEffect(() => {
+		async function getCourseCreator() {
+			if (connected) {
+				const _res = await getConnectedTokenAsset(
+					wallet,
+					andamioConfig.config.courseManagementTokens.courseCreatorPolicyID
+				);
+				if (_res) {
+					setConnectedCourseCreator(_res);
+				}
+			}
+		}
 
-    if (connected) {
-      getCourseCreator();
-    }
-  }, [connected, wallet]);
+		if (connected) {
+			getCourseCreator();
+		}
+	}, [connected, wallet]);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 
-  return (
-    <div>
-      {connectedCourseCreator && (
-        <>
-          <button onClick={openModal} className="btn btn-primary" type="button">
-            Mint this Module Token
-          </button>
-          <Modal
-            isOpen={isModalOpen}
-            closeModal={closeModal}
-            content={
-              <Suspense fallback={<Loading />}>
-                <MintCourseModuleToken courseModule={props.courseModule} closeModal={closeModal} />
-              </Suspense>
-            }
-          />
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{connectedCourseCreator && (
+				<>
+					<button
+						onClick={openModal}
+						className='btn btn-primary rounded-md drop-shadow'
+						type='button'>
+						Mint this Module Token
+					</button>
+					<Modal
+						isOpen={isModalOpen}
+						closeModal={closeModal}
+						content={
+							<Suspense fallback={<Loading />}>
+								<MintCourseModuleToken
+									courseModule={props.courseModule}
+									closeModal={closeModal}
+								/>
+							</Suspense>
+						}
+					/>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default MintModuleTokenModal;

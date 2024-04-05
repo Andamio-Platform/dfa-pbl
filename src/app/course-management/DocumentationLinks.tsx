09abdb584c5e71459298e2cfe2aba4919e17c4d5
Roberto M <robertom@Robertos-MBP.lan>
2024-04-05 13:31:31 -0400

    adds rounded border and white shadow

--- src/app/course-management/DocumentationLinks.tsx
+++ src/app/course-management/DocumentationLinks.tsx
@@ -1,52 +1,62 @@
 import Link from "next/link";
 
 export default function DocumentationLinks() {
-  return (
-    <>
-      {process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(",").includes("cm") && (
-        <div className="card bg-primary text-primary-content p-10 w-full mx-auto mb-5 font-mono grid grid-cols-1">
-          <div className="col-span-1">
-            <h1 className="pb-5 text-2xl">Documentation</h1>
-          </div>
-          <div className="col-span-1">
-            <header className="text-xl text-info py-2">AndamioJS</header>
-            <ul className="text-sm uppercase">
-              <li className="py-1">
-                <a href="https://www.npmjs.com/package/@andamiojs/core">NPM Package</a>
-              </li>
-              <li className="py-1">
-                <a href="https://andamiojs.andamio.io">Andamio JS Docs</a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-span-1">
-            <header className="text-xl text-info py-2">In Andamio PBL</header>
-            <ul className="text-sm uppercase">
-              <li className="py-1">
-                <Link href="https://www.andamio.io/course/module/101/overview">Module 101: How to Learn in Andamio</Link>
-              </li>
-              <li className="py-1">
-                <Link href="https://www.andamio.io/course/module/201/overview">Module 201: How to Teach With Andamio</Link>
-              </li>
-            </ul>
-          </div>
+	return (
+		<>
+			{process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(",").includes(
+				"cm"
+			) && (
+				<div className='card bg-primary text-primary-content p-10 w-full mx-auto mb-5 font-mono grid grid-cols-1 rounded-lg shadow-white shadow-sm'>
+					<div className='col-span-1'>
+						<h1 className='pb-5 text-2xl'>Documentation</h1>
+					</div>
+					<div className='col-span-1'>
+						<header className='text-xl text-info py-2'>AndamioJS</header>
+						<ul className='text-sm uppercase'>
+							<li className='py-1'>
+								<a href='https://www.npmjs.com/package/@andamiojs/core'>
+									NPM Package
+								</a>
+							</li>
+							<li className='py-1'>
+								<a href='https://andamiojs.andamio.io'>Andamio JS Docs</a>
+							</li>
+						</ul>
+					</div>
+					<div className='col-span-1'>
+						<header className='text-xl text-info py-2'>In Andamio PBL</header>
+						<ul className='text-sm uppercase'>
+							<li className='py-1'>
+								<Link href='https://www.andamio.io/course/module/101/overview'>
+									Module 101: How to Learn in Andamio
+								</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='https://www.andamio.io/course/module/201/overview'>
+									Module 201: How to Teach With Andamio
+								</Link>
+							</li>
+						</ul>
+					</div>
 
-          <div className="col-span-1">
-            <header className="text-xl text-info py-2">Inquire: Coming Soon</header>
-            <ul className="text-sm uppercase">
-              <li className="py-1">
-                <Link href="#">Schedule a Consultation</Link>
-              </li>
-              <li className="py-1">
-                <Link href="#">Join a Public Meeting</Link>
-              </li>
-              <li className="py-1">
-                <Link href="#">Read Blog</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      )}
-    </>
-  );
+					<div className='col-span-1'>
+						<header className='text-xl text-info py-2'>
+							Inquire: Coming Soon
+						</header>
+						<ul className='text-sm uppercase'>
+							<li className='py-1'>
+								<Link href='#'>Schedule a Consultation</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='#'>Join a Public Meeting</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='#'>Read Blog</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }

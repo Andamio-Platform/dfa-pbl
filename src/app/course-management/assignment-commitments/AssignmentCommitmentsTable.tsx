a5fd935ca039b10de6cedf1a1c9e1cdf9343878c
Roberto M <robertom@Robertos-MBP.lan>
2024-04-05 13:34:13 -0400

    changes text from sec to primary

--- src/app/course-management/assignment-commitments/AssignmentCommitmentsTable.tsx
+++ src/app/course-management/assignment-commitments/AssignmentCommitmentsTable.tsx
@@ -12,44 +12,45 @@ import LearnerActionButtons from "./LearnerActionButtons";
 export const dynamic = "force-dynamic";
 
 const AssignmentCommitmentsTable = async () => {
-  const AssignmentInfo = await queryAssignmentValidatorInfo(andamioConfig);
+	const AssignmentInfo = await queryAssignmentValidatorInfo(andamioConfig);
 
-
-  return (
-    <tbody>
-      <table className="table">
-        <thead>
-          <tr className="text-secondary-content">
-            <th className="">#</th>
-            <th className="">Learner Token</th>
-            <th className="">Module Id</th>
-            <th className="">Assignment Details</th>
-            <th className="">Assignment Info Status</th>
-            <th className="">Student Actions</th>
-            <th className="">Facilitator Actions</th>
-          </tr>
-        </thead>
-        {AssignmentInfo.utxos.map((assignment, index) => (
-          <tr key={index}>
-            <td className="px-6 py-4">{index + 1}</td>
-            <td className="px-6 py-4">{assignment.data?.learnerTokenName}</td>
-            <td className="px-6 py-4">{assignment.data?.moduleId}</td>
-            <td className="px-6 py-4">{assignment.data?.assignmentName}</td>
-            <td className="px-6 py-4">{assignment.data?.studentAssignmentInfo}</td>
-            <td className="gap-5 py-4">
-              <LearnerActionButtons assignment={assignment} />
-            </td>
-            <td className="gap-5 py-4">
-              <div className="flex flex-row gap-3">
-                {<AcceptAssignmentModal assignment={assignment} />}
-                {<DenyAssignmentModal assignment={assignment} />}
-              </div>
-            </td>
-          </tr>
-        ))}
-      </table>
-    </tbody>
-  );
+	return (
+		<tbody>
+			<table className='table'>
+				<thead>
+					<tr className='text-primary-content'>
+						<th className=''>#</th>
+						<th className=''>Learner Token</th>
+						<th className=''>Module Id</th>
+						<th className=''>Assignment Details</th>
+						<th className=''>Assignment Info Status</th>
+						<th className=''>Student Actions</th>
+						<th className=''>Facilitator Actions</th>
+					</tr>
+				</thead>
+				{AssignmentInfo.utxos.map((assignment, index) => (
+					<tr key={index} className='text-primary-content'>
+						<td className='px-6 py-4'>{index + 1}</td>
+						<td className='px-6 py-4'>{assignment.data?.learnerTokenName}</td>
+						<td className='px-6 py-4'>{assignment.data?.moduleId}</td>
+						<td className='px-6 py-4'>{assignment.data?.assignmentName}</td>
+						<td className='px-6 py-4'>
+							{assignment.data?.studentAssignmentInfo}
+						</td>
+						<td className='gap-5 py-4'>
+							<LearnerActionButtons assignment={assignment} />
+						</td>
+						<td className='gap-5 py-4'>
+							<div className='flex flex-row gap-3'>
+								{<AcceptAssignmentModal assignment={assignment} />}
+								{<DenyAssignmentModal assignment={assignment} />}
+							</div>
+						</td>
+					</tr>
+				))}
+			</table>
+		</tbody>
+	);
 };
 
 export default AssignmentCommitmentsTable;

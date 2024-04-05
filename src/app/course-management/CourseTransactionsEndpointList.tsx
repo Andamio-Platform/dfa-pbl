67e0ec97d82b8e598c90c5673d37625d398ee6a5
Roberto M <robertom@Robertos-MBP.lan>
2024-04-05 13:31:03 -0400

    adds rounded border and white shadow

--- src/app/course-management/CourseTransactionsEndpointList.tsx
+++ src/app/course-management/CourseTransactionsEndpointList.tsx
@@ -1,58 +1,74 @@
 import Link from "next/link";
 
 export default function CourseTransactionEndpointList() {
-  return (
-    <>
-      {process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(",").includes("cm") && (
-        <div className="card bg-primary text-primary-content p-10 w-full mx-auto font-mono grid grid-cols-1">
-          <div className="col-span-1">
-            <h1 className="pb-5 text-2xl">Andamio Instance Endpoints</h1>
-          </div>
-          <div className="col-span-1">
-            <header className="text-xl text-info py-2">Learners</header>
-            <ul className="text-sm uppercase">
-              <li className="py-1">
-                <Link href="/course-management/roles/learner/mint">Mint a Learner Token</Link>
-              </li>
-              <li className="py-1">
-                <Link href="/course-management/module-list">Commit to a Module Assignment</Link>
-              </li>
-              <li className="py-1">
-                <Link href="/course-management/roles/learner/dashboard">Submit Assignment</Link>
-              </li>
-              <li className="py-1">
-                <Link href="/course-management/roles/learner/dashboard">Update Your Learner Token</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="col-span-1">
-            <header className="text-xl text-info py-2">Course Facilitators</header>
-            <ul className="text-sm uppercase">
-              <li className="py-1">
-                <Link href="/course-management/assignment-commitments">Accept an Assignment</Link>
-              </li>
-              <li className="py-1">
-                <Link href="/course-management/assignment-commitments">Deny an Assignment</Link>
-              </li>
-            </ul>
-          </div>
+	return (
+		<>
+			{process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(",").includes(
+				"cm"
+			) && (
+				<div className='card bg-primary text-primary-content p-10 w-full mx-auto font-mono grid grid-cols-1 rounded-lg shadow-white shadow-sm'>
+					<div className='col-span-1'>
+						<h1 className='pb-5 text-2xl'>Andamio Instance Endpoints</h1>
+					</div>
+					<div className='col-span-1'>
+						<header className='text-xl text-info py-2'>Learners</header>
+						<ul className='text-sm uppercase'>
+							<li className='py-1'>
+								<Link href='/course-management/roles/learner/mint'>
+									Mint a Learner Token
+								</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='/course-management/module-list'>
+									Commit to a Module Assignment
+								</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='/course-management/roles/learner/dashboard'>
+									Submit Assignment
+								</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='/course-management/roles/learner/dashboard'>
+									Update Your Learner Token
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<div className='col-span-1'>
+						<header className='text-xl text-info py-2'>
+							Course Facilitators
+						</header>
+						<ul className='text-sm uppercase'>
+							<li className='py-1'>
+								<Link href='/course-management/assignment-commitments'>
+									Accept an Assignment
+								</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='/course-management/assignment-commitments'>
+									Deny an Assignment
+								</Link>
+							</li>
+						</ul>
+					</div>
 
-          <div className="col-span-1">
-            <header className="text-xl text-info py-2">Course Creators</header>
-            <ul className="text-sm uppercase">
-              <li className="py-1">
-                <Link href="/course">Mint a Course Module</Link>
-              </li>
-              <li className="py-1">
-                <Link href="/course">Update a Course Module</Link>
-              </li>
-              <li className="py-1">
-                <Link href="/course">Burn a Course Module</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      )}
-    </>
-  );
+					<div className='col-span-1'>
+						<header className='text-xl text-info py-2'>Course Creators</header>
+						<ul className='text-sm uppercase'>
+							<li className='py-1'>
+								<Link href='/course'>Mint a Course Module</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='/course'>Update a Course Module</Link>
+							</li>
+							<li className='py-1'>
+								<Link href='/course'>Burn a Course Module</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }

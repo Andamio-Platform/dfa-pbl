ebc10af0d5b13077e60a1ea1868c423942ceddb9
Roberto M <robertom@Robertos-MBP.lan>
2024-04-05 13:46:13 -0400

    adds button styling

--- src/components/modals/course/StudentRemoveAssignmentModal.tsx
+++ src/components/modals/course/StudentRemoveAssignmentModal.tsx
@@ -6,37 +6,41 @@ import { AssignmentUTxO } from "@andamiojs/core";
 import Loading from "../../../app/Loading";
 import StudentRemoveAssignment from "../../transactions/course/StudentRemoveAssignment";
 
-const StudentRemoveAssignmentModal = (props: { assignment: AssignmentUTxO }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const StudentRemoveAssignmentModal = (props: {
+	assignment: AssignmentUTxO;
+}) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 
-  return (
-    <div>
-      <button
-        onClick={openModal}
-        className="btn btn-error btn-sm"
-        type="button"
-      >
-        Remove
-      </button>
-      <Modal
-        isOpen={isModalOpen}
-        closeModal={closeModal}
-        content={
-          <Suspense fallback={<Loading />}>
-            <StudentRemoveAssignment assignment={props.assignment} closeModal={closeModal} />
-          </Suspense>
-        }
-      />
-    </div>
-  );
+	return (
+		<div>
+			<button
+				onClick={openModal}
+				className='btn btn-error btn-sm rounded-md drop-shadow text-black'
+				type='button'>
+				Remove
+			</button>
+			<Modal
+				isOpen={isModalOpen}
+				closeModal={closeModal}
+				content={
+					<Suspense fallback={<Loading />}>
+						<StudentRemoveAssignment
+							assignment={props.assignment}
+							closeModal={closeModal}
+						/>
+					</Suspense>
+				}
+			/>
+		</div>
+	);
 };
 
 export default StudentRemoveAssignmentModal;

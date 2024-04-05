22e55445351f50a5d59f3a9fb0ad931ec91cdeab
Roberto M <robertom@Robertos-MBP.lan>
2024-04-05 13:42:25 -0400

    adds button styling

--- src/components/modals/course/MintLearnerTokenModal.tsx
+++ src/components/modals/course/MintLearnerTokenModal.tsx
@@ -7,32 +7,35 @@ import Modal from "../Modal";
 import MintLearnerToken from "../../transactions/course/MintLearnerToken";
 
 const MintLearnerTokenModal = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 
-  return (
-    <div>
-      <button onClick={openModal} className="btn btn-success" type="button">
-        Mint Your Learner Token
-      </button>
-      <Modal
-        isOpen={isModalOpen}
-        closeModal={closeModal}
-        content={
-          <Suspense fallback={<Loading />}>
-            <MintLearnerToken closeModal={closeModal} />
-          </Suspense>
-        }
-      />
-    </div>
-  );
+	return (
+		<div>
+			<button
+				onClick={openModal}
+				className='btn btn-success rounded-md drop-shadow text-black rounded-lg hover:scale-105 mx-auto'
+				type='button'>
+				Mint Your Learner Token
+			</button>
+			<Modal
+				isOpen={isModalOpen}
+				closeModal={closeModal}
+				content={
+					<Suspense fallback={<Loading />}>
+						<MintLearnerToken closeModal={closeModal} />
+					</Suspense>
+				}
+			/>
+		</div>
+	);
 };
 
 export default MintLearnerTokenModal;

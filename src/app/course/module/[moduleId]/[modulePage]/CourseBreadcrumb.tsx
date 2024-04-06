5ac7d0c842231ba7eedb8fce6008d70deb8259c1
Roberto M <robertom@Robertos-MBP.lan>
2024-04-06 14:00:23 -0400

    button stylying changes, rounded and drop shadow

--- src/app/course/module/[moduleId]/[modulePage]/CourseBreadcrumb.tsx
+++ src/app/course/module/[moduleId]/[modulePage]/CourseBreadcrumb.tsx
@@ -2,18 +2,19 @@ import Link from "next/link";
 import { andamioConfig } from "../../../../../andamio/config";
 
 export default function CourseBreadcrumb() {
-
-  return (
-    <div className="card bg-secondary text-secondary-content shadow-xl px-4 py-2 font-mono">
-      <div className="breadcrumbs">
-        <ul>
-          <li>
-            <Link href="/course" className="btn btn-sm btn-info text-sm uppercase">
-              {andamioConfig.title}
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className='card bg-secondary text-secondary-content shadow-xl px-4 py-2 font-mono'>
+			<div className='breadcrumbs'>
+				<ul>
+					<li>
+						<Link
+							href='/course'
+							className='btn btn-sm btn-info text-sm rounded-lg drop-shadow uppercase'>
+							{andamioConfig.title}
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }

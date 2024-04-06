c6572b670197c5c8b4323e4abef42cb816e200e2
Roberto M <robertom@Robertos-MBP.lan>
2024-04-06 14:04:09 -0400

    styling changes, rounded borders and drop shadow, button style changes, changed success button conditional rendering to li tag instead of link

--- src/app/course/module/[moduleId]/[modulePage]/ModuleBreadcrumbs.tsx
+++ src/app/course/module/[moduleId]/[modulePage]/ModuleBreadcrumbs.tsx
@@ -4,45 +4,46 @@ import { LMSObjectMetadata } from "@andamiojs/core";
 import { andamioConfig } from "../../../../../andamio/config";
 
 export default function ModuleBreadcrumbs({
-  moduleId,
-  frontmatter,
+	moduleId,
+	frontmatter
 }: {
-  moduleId: string | undefined;
-  frontmatter: LMSObjectMetadata | undefined;
+	moduleId: string | undefined;
+	frontmatter: LMSObjectMetadata | undefined;
 }) {
-
-  return (
-    <div className="card bg-secondary text-secondary-content shadow-xl px-4 py-2 font-mono">
-      <div className="breadcrumbs">
-        <ul>
-          <li>
-            <Link href="/course" className="btn btn-sm btn-info text-sm uppercase">
-              {andamioConfig.title}
-            </Link>
-          </li>
-          {moduleId && frontmatter && (
-            <>
-              <li>
-                <Link
-                  href={`/course/module/${moduleId}`}
-                  className={`btn btn-sm text-sm uppercase ${
-                    frontmatter.type == "ModuleOverview" ? "btn-success" : "btn-info"
-                  }`}
-                >
-                  Module {moduleId}: {getModuleTitle({ moduleId })}
-                </Link>
-              </li>
-              {frontmatter.type != "ModuleOverview" && (
-                <li>
-                  <Link href={`#`} className="btn btn-sm btn-success text-sm uppercase">
-                    {frontmatter.title}
-                  </Link>
-                </li>
-              )}
-            </>
-          )}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className='card bg-secondary text-secondary-content rounded-lg drop-shadow px-4 py-2 font-mono'>
+			<div className='breadcrumbs'>
+				<ul>
+					<li className='btn btn-info rounded-lg drop-shadow ml-5 hover:scale-105'>
+						<Link href='/course' className='text-sm uppercase'>
+							{andamioConfig.title}
+						</Link>
+					</li>
+					{moduleId && frontmatter && (
+						<>
+							<li
+								className={`btn rounded-lg drop-shadow ml-5 hover:scale-105 ${
+									frontmatter.type == "ModuleOverview"
+										? "btn-success"
+										: "btn-info"
+								}`}>
+								<Link
+									href={`/course/module/${moduleId}`}
+									className='text-sm uppercase'>
+									Module {moduleId}: {getModuleTitle({ moduleId })}
+								</Link>
+							</li>
+							{frontmatter.type != "ModuleOverview" && (
+								<li className='btn btn-success rounded-lg drop-shadow ml-8 hover:scale-105'>
+									<Link href={`#`} className='text-sm uppercase'>
+										{frontmatter.title}
+									</Link>
+								</li>
+							)}
+						</>
+					)}
+				</ul>
+			</div>
+		</div>
+	);
 }

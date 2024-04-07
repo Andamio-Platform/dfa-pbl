a341df00b97340906447cd4ad35d5dd4b76742e6
Roberto M <robertom@Robertos-MBP.lan>
2024-04-07 09:48:45 -0400

    adds z-40 to navbar

--- src/components/ui/GlobalNav.tsx
+++ src/components/ui/GlobalNav.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { CardanoWallet } from '@meshsdk/react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { andamioConfig } from '../../andamio/config';
+import { CardanoWallet } from "@meshsdk/react";
+import Image from "next/image";
+import Link from "next/link";
+import { andamioConfig } from "../../andamio/config";
 
 const GlobalNav = () => (
-	<div className='card text-primary-content font-mono shadow-xl mx-5 mt-5 z-10'>
+	<div className='card text-primary-content font-mono shadow-xl mx-5 mt-5 z-40'>
 		<div className='navbar'>
 			<div className='flex-1'>
 				<Link className='btn btn-ghost normal-case text-lg' href='/'>
@@ -21,29 +21,29 @@ const GlobalNav = () => (
 			</div>
 			<ul className='menu menu-horizontal px-1'>
 				<li>
-					<Link href={'/course'}>Start Course</Link>
+					<Link href={"/course"}>Start Course</Link>
 				</li>
 				<li>
-					<Link href={'/course-management/roles/learner/dashboard'}>
+					<Link href={"/course-management/roles/learner/dashboard"}>
 						Learner Dashboard
 					</Link>
 				</li>
-				{process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(',').includes(
-					'cm'
+				{process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(",").includes(
+					"cm"
 				) && (
 					<li>
-						<Link href={'/course-management'}>Course Management</Link>
+						<Link href={"/course-management"}>Course Management</Link>
 					</li>
 				)}
-				{process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(',').includes(
-					'pm'
+				{process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(",").includes(
+					"pm"
 				) && (
 					<li>
-						<Link href={'/contributor-platform'}>Contributor Platform</Link>
+						<Link href={"/contributor-platform"}>Contributor Platform</Link>
 					</li>
 				)}
 			</ul>
-			<div className='ml-5'>
+			<div className='ml-5 z-40'>
 				<CardanoWallet />
 			</div>
 		</div>

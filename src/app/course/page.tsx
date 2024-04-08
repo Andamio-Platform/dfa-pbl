92a97df02b99b3de20e4dbbc7c1e04528f5a0a5e
Roberto M <robertom@Robertos-MBP.lan>
2024-04-08 17:08:10 -0400

    commented out styles and CourseOutline since they are not being used

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,25 +1,27 @@
 import LessonList from "./LessonList";
 import { Suspense } from "react";
 import Loading from "../Loading";
-import styles from "./CoursePage.module.css";
-import CourseOutline from "./CourseOutline";
+// import styles from "./CoursePage.module.css";
+// import CourseOutline from "./CourseOutline";
 import ModuleBreadcrumbs from "./module/[moduleId]/[modulePage]/ModuleBreadcrumbs";
 
 export const dynamic = "force-dynamic";
 
 export default async function CoursePage() {
-  const AllLessons = await LessonList();
-  // const courseOutline = await CourseOutline();
+	const AllLessons = await LessonList();
+	// const courseOutline = await CourseOutline();
 
-  return (
-    <main className="items-center justify-center font-extrabold text-4xl" style={{ minHeight: "calc(100vh - 5rem)" }}>
-      <div className="mb-5">
-        <ModuleBreadcrumbs frontmatter={undefined} moduleId={undefined} />
-      </div>
-      {/* <div className={styles.coursePageContainer}>
+	return (
+		<main
+			className='items-center justify-center font-extrabold text-4xl'
+			style={{ minHeight: "calc(100vh - 5rem)" }}>
+			<div className='mb-5'>
+				<ModuleBreadcrumbs frontmatter={undefined} moduleId={undefined} />
+			</div>
+			{/* <div className={styles.coursePageContainer}>
         <Suspense fallback={<Loading />}>{courseOutline}</Suspense>
       </div> */}
-      <Suspense fallback={<Loading />}>{AllLessons}</Suspense>
-    </main>
-  );
+			<Suspense fallback={<Loading />}>{AllLessons}</Suspense>
+		</main>
+	);
 }

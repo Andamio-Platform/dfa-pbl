1134f9b25e5e8fa20cce1bf19c730b0288b13882
Roberto M <robertom@Robertos-MBP.lan>
2024-04-08 10:40:27 -0400

    adds button styling

--- src/components/transactions/course/BurnCourseModuleToken.tsx
+++ src/components/transactions/course/BurnCourseModuleToken.tsx
@@ -1,52 +1,66 @@
 import { useWallet } from "@meshsdk/react";
-import { CourseReferenceUTxO, prepareBurnCourseModuleTokenTx } from "@andamiojs/core";
+import {
+	CourseReferenceUTxO,
+	prepareBurnCourseModuleTokenTx
+} from "@andamiojs/core";
 import { useState } from "react";
 import { andamioConfig } from "../../../andamio/config";
 import ErrorModal from "../../modals/ErrorModal";
 import SuccessTxModal from "../../modals/SuccessTxModal";
 
-const BurnCourseModuleToken = (props: { selectedModuleUTxO: CourseReferenceUTxO; closeModal: () => void }) => {
-  const [txHash, setTxHash] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const { wallet, connected } = useWallet();
-  if (connected) {
-    const handleClick = async () => {
-      try {
-        const BURN_COURSE_MODULE_TOKEN = await prepareBurnCourseModuleTokenTx(
-          wallet,
-          andamioConfig,
-          props.selectedModuleUTxO
-        );
-        const res = await BURN_COURSE_MODULE_TOKEN.runTx();
+const BurnCourseModuleToken = (props: {
+	selectedModuleUTxO: CourseReferenceUTxO;
+	closeModal: () => void;
+}) => {
+	const [txHash, setTxHash] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const { wallet, connected } = useWallet();
+	if (connected) {
+		const handleClick = async () => {
+			try {
+				const BURN_COURSE_MODULE_TOKEN = await prepareBurnCourseModuleTokenTx(
+					wallet,
+					andamioConfig,
+					props.selectedModuleUTxO
+				);
+				const res = await BURN_COURSE_MODULE_TOKEN.runTx();
 
-        setTxHash(res);
-      } catch (error) {
-        if (error instanceof Error) {
-          setErrorMessage(error.message);
-        } else {
-          setErrorMessage(JSON.stringify(error));
-        }
-      }
-    };
-    return (
-      <>
-        {txHash !== "" ? <SuccessTxModal txHash={txHash} closeModal={props.closeModal} /> : null}
-        {errorMessage !== "" ? <ErrorModal errorMessage={errorMessage} closeModal={props.closeModal} /> : null}
-        <div className="flex flex-col py-10 items-center bg-gradient-br w-max p-24">
-          <div className="font-extrabold mb-4 text-xl">You will burn this module token</div>
+				setTxHash(res);
+			} catch (error) {
+				if (error instanceof Error) {
+					setErrorMessage(error.message);
+				} else {
+					setErrorMessage(JSON.stringify(error));
+				}
+			}
+		};
+		return (
+			<>
+				{txHash !== "" ? (
+					<SuccessTxModal txHash={txHash} closeModal={props.closeModal} />
+				) : null}
+				{errorMessage !== "" ? (
+					<ErrorModal
+						errorMessage={errorMessage}
+						closeModal={props.closeModal}
+					/>
+				) : null}
+				<div className='flex flex-col py-10 items-center bg-gradient-br w-max p-24'>
+					<div className='font-extrabold mb-4 text-xl'>
+						You will burn this module token
+					</div>
 
-          <button
-            onClick={handleClick}
-            className="btn btn-sm btn-sm"
-            type="button"
-          >
-            Confirm Burn Module Token
-          </button>
-        </div>
-      </>
-    );
-  }
-  return <>Wallet not connected</>;
+					<button
+						onClick={handleClick}
+						className='btn btn-error p-3 rounded-lg drop-shadow hover:bg-primary hover:text-primary-content hover:scale-105'
+						type='button'>
+						Confirm Burn
+					</button>
+				</div>
+			</>
+		);
+	}
+	return <>Wallet not connected</>;
 };
 
 export default BurnCourseModuleToken;

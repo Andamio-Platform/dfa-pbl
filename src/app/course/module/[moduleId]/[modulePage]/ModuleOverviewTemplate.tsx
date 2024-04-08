95c3a89363627df6912a10bd81641764c5eb32ce
Roberto M <robertom@Robertos-MBP.lan>
2024-04-08 17:07:10 -0400

    adds responsive max-width to lesson and styling to right side navigation links

--- src/app/course/module/[moduleId]/[modulePage]/ModuleOverviewTemplate.tsx
+++ src/app/course/module/[moduleId]/[modulePage]/ModuleOverviewTemplate.tsx
@@ -21,42 +21,44 @@ export default async function ModuleOverviewTemplate({
 		<>
 			<div className={styles.coursePageContainer}>
 				<div className='card bg-secondary text-secondary-content shadow-xl p-5 rounded-lg drop-shadow'>
-					<div className='grid grid-cols-3 py-3'>
-						<header className='col-span-2'>
-							<h3 className='text-accent'>Welcome to Module {moduleId}</h3>
-							<h1 className=''>{moduleTitle}</h1>
-							<div className='my-5'>
-								<h4 className=''>Student Learning Targets</h4>
-								{sltList.map((slt: SLT) => (
-									<div className={styles.slt} key={slt.id}>
-										<span className='text-info'>{slt.id}:</span> {slt.slt}
+					<div className='max-w-screen-md lg:max-w-screen-xl mx-auto py-5'>
+						<div className='grid grid-cols-3 py-3'>
+							<header className='col-span-2'>
+								<h3 className='text-accent'>Welcome to Module {moduleId}</h3>
+								<h1 className=''>{moduleTitle}</h1>
+								<div className='my-5'>
+									<h4 className=''>Student Learning Targets</h4>
+									{sltList.map((slt: SLT) => (
+										<div className={styles.slt} key={slt.id}>
+											<span className='text-info'>{slt.id}:</span> {slt.slt}
+										</div>
+									))}
+								</div>
+							</header>
+							<div className='grid w-5/6 mx-auto'>
+								{selectedModuleOverview?.pages.map((page) => (
+									<div className='' key={page.slug}>
+										{page.type != "ModuleOverview" && (
+											<Link
+												href={`/course/module/${selectedModuleOverview.id}/${page.slug}`}>
+												<div className='hover:bg-info hover:rounded-lg hover:drop-shadow hover:scale-105 text-secondary-content p-3 mb-5'>
+													<div className='mb-3 pb-1 border-b border-primary text-xl'>
+														{page.title}
+													</div>
+													<div className='font-bold'>{page.description}</div>
+													<div className='text-sm'>
+														Edited on: {page.lastEdited}
+													</div>
+												</div>
+											</Link>
+										)}
 									</div>
 								))}
 							</div>
-						</header>
-						<div className='grid w-5/6 mx-auto'>
-							{selectedModuleOverview?.pages.map((page) => (
-								<div className='' key={page.slug}>
-									{page.type != "ModuleOverview" && (
-										<Link
-											href={`/course/module/${selectedModuleOverview.id}/${page.slug}`}>
-											<div className='hover:bg-info text-secondary-content p-3 mb-5'>
-												<div className='mb-3 pb-1 border-b border-primary text-xl'>
-													{page.title}
-												</div>
-												<div className='font-bold'>{page.description}</div>
-												<div className='text-sm'>
-													Edited on: {page.lastEdited}
-												</div>
-											</div>
-										</Link>
-									)}
-								</div>
-							))}
 						</div>
+						<hr className='my-10' />
+						<div>{children}</div>
 					</div>
-					<hr className='my-10' />
-					<div>{children}</div>
 				</div>
 			</div>
 		</>

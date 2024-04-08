43e2b4752bfbdb975c707821427b142e5bbe6221
Roberto M <robertom@Robertos-MBP.lan>
2024-04-08 10:39:29 -0400

    adds button styling

--- src/components/modals/course/CommitToAssignmentModal.tsx
+++ src/components/modals/course/CommitToAssignmentModal.tsx
@@ -9,55 +9,64 @@ import { useWallet } from "@meshsdk/react";
 import { AssetExtended } from "@meshsdk/core";
 import { andamioConfig } from "../../../andamio/config";
 
-const CommitToAssignmentModal = (props: { selectedModuleUTxO: CourseReferenceUTxO }) => {
-  const { connected, wallet } = useWallet();
-  const [connectedLearner, setConnectedLearner] = useState<AssetExtended | undefined>(undefined);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const CommitToAssignmentModal = (props: {
+	selectedModuleUTxO: CourseReferenceUTxO;
+}) => {
+	const { connected, wallet } = useWallet();
+	const [connectedLearner, setConnectedLearner] = useState<
+		AssetExtended | undefined
+	>(undefined);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    async function getLearnerToken() {
-      if (connected) {
-        const _res = await getConnectedTokenAsset(wallet, andamioConfig.config.courseManagementTokens.learnerPolicyID);
-        setConnectedLearner(_res);
-      }
-    }
+	useEffect(() => {
+		async function getLearnerToken() {
+			if (connected) {
+				const _res = await getConnectedTokenAsset(
+					wallet,
+					andamioConfig.config.courseManagementTokens.learnerPolicyID
+				);
+				setConnectedLearner(_res);
+			}
+		}
 
-    if (connected) {
-      getLearnerToken();
-    }
-  }, [connected, wallet]);
+		if (connected) {
+			getLearnerToken();
+		}
+	}, [connected, wallet]);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 
-  return (
-    <div>
-      {connectedLearner && (
-        <>
-          <button
-            onClick={openModal}
-            className="btn btn-sm btn-info hover:scale-105"
-          >
-            Commit to Assignment
-          </button>
-          <Modal
-            isOpen={isModalOpen}
-            closeModal={closeModal}
-            content={
-              <Suspense fallback={<Loading />}>
-                <CommitToAssignment selectedModuleUTxO={props.selectedModuleUTxO} closeModal={closeModal} />
-              </Suspense>
-            }
-          />
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{connectedLearner && (
+				<>
+					<button
+						onClick={openModal}
+						className='btn bg-info text-primary rounded-lg drop-shadow hover:bg-primary hover:text-primary-content hover:scale-105 hover:border-solid hover:border-white'>
+						Commit to Assignment
+					</button>
+					<Modal
+						isOpen={isModalOpen}
+						closeModal={closeModal}
+						content={
+							<Suspense fallback={<Loading />}>
+								<CommitToAssignment
+									selectedModuleUTxO={props.selectedModuleUTxO}
+									closeModal={closeModal}
+								/>
+							</Suspense>
+						}
+					/>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default CommitToAssignmentModal;

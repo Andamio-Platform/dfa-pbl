5a3bb8046e4bd0056dc1163211ddba8d1e17e8e7
Roberto M <robertom@Robertos-MBP.lan>
2024-04-09 13:01:59 -0400

    adds button styling

--- src/components/modals/course/BurnLearnerReferenceTokensModal.tsx
+++ src/components/modals/course/BurnLearnerReferenceTokensModal.tsx
@@ -10,56 +10,72 @@ import BurnLearnerTokens from "../../transactions/course/BurnLearnerTokens";
 import Modal from "../Modal";
 
 // Todo change type name from Module to CourseModule?
-const BurnLearnerReferenceTokensModal = (props: { learnerReferenceUTxO: LearnerReferenceUTxO }) => {
-  const { connected, wallet } = useWallet();
-  const [connectedLearner, setConnectedLearner] = useState<AssetExtended | undefined>(undefined);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const BurnLearnerReferenceTokensModal = (props: {
+	learnerReferenceUTxO: LearnerReferenceUTxO;
+}) => {
+	const { connected, wallet } = useWallet();
+	const [connectedLearner, setConnectedLearner] = useState<
+		AssetExtended | undefined
+	>(undefined);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    async function getLearnerToken() {
-      if (connected) {
-        const _res = await getConnectedTokenAsset(wallet, andamioConfig.config.courseManagementTokens.learnerPolicyID);
-        setConnectedLearner(_res);
-      }
-    }
+	useEffect(() => {
+		async function getLearnerToken() {
+			if (connected) {
+				const _res = await getConnectedTokenAsset(
+					wallet,
+					andamioConfig.config.courseManagementTokens.learnerPolicyID
+				);
+				setConnectedLearner(_res);
+			}
+		}
 
-    if (connected) {
-      getLearnerToken();
-    }
-  }, [connected, wallet]);
+		if (connected) {
+			getLearnerToken();
+		}
+	}, [connected, wallet]);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 
-  return (
-    <div>
-      {connectedLearner && (
-        <>
-          <button onClick={openModal} className="btn btn-error btn-sm my-2" type="button">
-            Burn Your Learner Token
-          </button>
-          <Modal
-            isOpen={isModalOpen}
-            closeModal={closeModal}
-            content={
-              <Suspense fallback={<Loading />}>
-                <div className="text-sm">
-                  <h1>Burn Learner Tokens</h1>
-                  <p>Burning Tokens for: {props.learnerReferenceUTxO.data?.contributorAlias}</p>
-                  <BurnLearnerTokens learnerReferenceUTxO={props.learnerReferenceUTxO} closeModal={closeModal} />
-                </div>
-              </Suspense>
-            }
-          />
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{connectedLearner && (
+				<>
+					<button
+						onClick={openModal}
+						className='btn btn-error btn-md my-2 rounded-lg drop-shadow hover:bg-primary hover:text-primary-content hover:scale-105'
+						type='button'>
+						Burn Your Learner Token
+					</button>
+					<Modal
+						isOpen={isModalOpen}
+						closeModal={closeModal}
+						content={
+							<Suspense fallback={<Loading />}>
+								<div className='text-sm'>
+									<h1>Burn Learner Tokens</h1>
+									<p>
+										Burning Tokens for:{" "}
+										{props.learnerReferenceUTxO.data?.contributorAlias}
+									</p>
+									<BurnLearnerTokens
+										learnerReferenceUTxO={props.learnerReferenceUTxO}
+										closeModal={closeModal}
+									/>
+								</div>
+							</Suspense>
+						}
+					/>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default BurnLearnerReferenceTokensModal;

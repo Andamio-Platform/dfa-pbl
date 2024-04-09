6600dde4b1f8c3f7392a1dc014ad94a7f1e0793a
Roberto M <robertom@Robertos-MBP.lan>
2024-04-09 12:27:04 -0400

    adds DFA learner token image to config

--- src/andamio/config/index.ts
+++ src/andamio/config/index.ts
@@ -1,4 +1,8 @@
-import { AndamioConfig, CourseManagementConfig, ProjectManagementConfig } from "@andamiojs/core";
+import {
+	AndamioConfig,
+	CourseManagementConfig,
+	ProjectManagementConfig
+} from "@andamiojs/core";
 
 // Project Management
 // import { escrows } from "./escrow";
@@ -11,44 +15,45 @@ import { AndamioConfig, CourseManagementConfig, ProjectManagementConfig } from "
 // import escrowDecider2 from "../cardano/plutus/escrowDecider2.json"
 
 // Course Management
-import assignment from "../cardano/plutus/assignment.json"
-import courseReference from "../cardano/plutus/courseReference.json"
-import learnerReference from "../cardano/plutus/learnerReference.json"
-import learnerMintingReference from "../cardano/plutus/learnerMintingReference.json"
-import moduleMintingReference from "../cardano/plutus/moduleMintingReference.json"
+import assignment from "../cardano/plutus/assignment.json";
+import courseReference from "../cardano/plutus/courseReference.json";
+import learnerReference from "../cardano/plutus/learnerReference.json";
+import learnerMintingReference from "../cardano/plutus/learnerMintingReference.json";
+import moduleMintingReference from "../cardano/plutus/moduleMintingReference.json";
 
 // Tokens
-import courseManagementTokens from './courseManagementTokens.json'
+import courseManagementTokens from "./courseManagementTokens.json";
 // import projectManagementTokens from './projectManagementTokens.json'
 
 export const andamioConfig: AndamioConfig<CourseManagementConfig> = {
-  title: "Deep Funding Academy PBL",
-  baseAddress: "",
-  enterpriseAddress: "",
-  rewardAddress: "",
-  // at the moment, each contract has a different reference UTxO Address - see individual contract configs
-  // referenceScriptAddress: '',
-  metadataKey: "",
-  network: "0",
-  config: {
-    assignment: assignment,
-    courseReference: courseReference,
-    learnerReference: learnerReference,
-    learnerMintingReference: learnerMintingReference,
-    moduleMintingReference: moduleMintingReference,
-    courseManagementTokens: courseManagementTokens,
-    coursemanagementNftArtwork: {
-      // move to a file?
-      learnerNFTURL: "https://www.andamio.io/andamio.png",
-      courseModuleNFTURL: "https://www.andamio.io/andamio.png",
-      courseCreatorNFTURL: "https://www.andamio.io/andamio.png",
-      courseDeciderNFTURL: "https://www.andamio.io/andamio.png",
-    },
-    // contributorReference: contributorReference,
-    // escrows: [escrowDecider1, escrowDecider2],
-    // treasury: treasury,
-    // contractTokenMintingReference: contractTokenMintingReference,
-    // contributorMintingReference: contributorMintingReference,
-    // projectManagementTokens: projectManagementTokens,
-  },
+	title: "Deep Funding Academy PBL",
+	baseAddress: "",
+	enterpriseAddress: "",
+	rewardAddress: "",
+	// at the moment, each contract has a different reference UTxO Address - see individual contract configs
+	// referenceScriptAddress: '',
+	metadataKey: "",
+	network: "0",
+	config: {
+		assignment: assignment,
+		courseReference: courseReference,
+		learnerReference: learnerReference,
+		learnerMintingReference: learnerMintingReference,
+		moduleMintingReference: moduleMintingReference,
+		courseManagementTokens: courseManagementTokens,
+		coursemanagementNftArtwork: {
+			// move to a file?
+			learnerNFTURL:
+				"[url=https://ibb.co/FsVj0QJ][img]https://i.ibb.co/FsVj0QJ/DFA-PBL1-Learner.png[/img][/url]",
+			courseModuleNFTURL: "https://www.andamio.io/andamio.png",
+			courseCreatorNFTURL: "https://www.andamio.io/andamio.png",
+			courseDeciderNFTURL: "https://www.andamio.io/andamio.png"
+		}
+		// contributorReference: contributorReference,
+		// escrows: [escrowDecider1, escrowDecider2],
+		// treasury: treasury,
+		// contractTokenMintingReference: contractTokenMintingReference,
+		// contributorMintingReference: contributorMintingReference,
+		// projectManagementTokens: projectManagementTokens,
+	}
 };

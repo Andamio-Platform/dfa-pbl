79f5e01746575a7745611c6c6829d61493f7a7c5
Roberto M <robertom@Robertos-MBP.lan>
2024-04-09 12:28:58 -0400

    changes text color to white

--- src/app/course-management/roles/learner/dashboard/page.tsx
+++ src/app/course-management/roles/learner/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function LearnerDashboardPage() {
 		<>
 			<div className="w-2/3 mx-auto fixed right-0 min-h-screen bg-[url('/icons/scaffold.svg')] z-0 opacity-40" />
 			<main className='flex flex-col w-3/4 mx-auto min-h-screen'>
-				<h1 className='text-3xl font-mono font-bold text-accent mt-10'>
+				<h1 className='text-3xl font-mono font-bold text-primary-content mt-10'>
 					{andamioConfig.title}
 				</h1>
 				<h1 className='text-6xl font-mono text-primary-content mb-10'>
